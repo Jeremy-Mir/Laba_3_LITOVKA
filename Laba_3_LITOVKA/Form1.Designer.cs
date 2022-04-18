2b678e150e647a7c260ccf1ee9764911c239b4ea
Your Name <elik@example.com>
2022-04-18 16:05:33 +0300

    123

--- Laba_3_LITOVKA/Form1.Designer.cs
+++ Laba_3_LITOVKA/Form1.Designer.cs
@@ -29,13 +29,87 @@ namespace Laba_3_LITOVKA
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Lime;
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Color = System.Drawing.Color.Red;
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(530, 551);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(587, 12);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Series1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Color = System.Drawing.Color.Lime;
+            series5.Name = "Series2";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Color = System.Drawing.Color.Red;
+            series6.Name = "Series3";
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(530, 551);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1129, 575);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
     }
 }
 
--- Laba_3_LITOVKA/Form1.cs
+++ Laba_3_LITOVKA/Form1.cs
@@ -12,9 +12,107 @@ namespace Laba_3_LITOVKA
 {
     public partial class Form1 : Form
     {
+        static double R = 8.31;
+        static double E1 = 251000;
+        static double E2 = 297000;
+        static double A1 = 200000000000;
+        static double A2 = 8000000000000;
+        static double D = 0.1;
+        static double Cj = 31 * 1.4 / 100/ 0.028;
+        static double T = 1350;
+        static double L = 160;
+        static double m0 = 2.6;
+        static double m1 = 3.0;
+        static double k1 = A1 * Math.Exp(-E1 / R / T);
+        static double k2 = A2 * Math.Exp(-E2 / R / T);
+        static double p = 1.4;
+
+        public double u(double m)
+        {
+            return m / (p * 3.14 * Math.Pow(D, 2) / 4);
+        }
+        public double d_C1( double C, double m)
+        {
+            return -k1 * C / u(m);
+        }
+        public double d_C2(double C1, double C2, double m)
+        {
+            return (k1 * C1 - k2 * C2 )/ u(m);
+        }
         public Form1()
         {
             InitializeComponent();
+
+            double C = Cj;
+            double m = m0;
+            chart1.Series[0].Points.Clear();
+            for (double l = 0; l < L; l = l + 0.5)
+            {
+                chart1.Series[0].Points.AddXY(l, C);
+                C = C + d_C1(C,m) * 0.5;
+
+            }
+            C = Cj;
+            m = m + 0.15;
+            chart1.Series[1].Points.Clear();
+            for (double l = 0; l < L; l = l + 0.5)
+            {
+                chart1.Series[1].Points.AddXY(l, C);
+                C = C + d_C1(C, m) * 0.5;
+
+            }
+            
+            C = Cj;
+            m = m + 0.15;
+            chart1.Series[2].Points.Clear();
+            for (double l = 0; l < L; l = l + 0.5)
+            {
+                chart1.Series[2].Points.AddXY(l, C);
+                C = C + d_C1(C, m) * 0.5;
+
+            }
+//chart1.ChartAreas[0].AxisX.Maximum = 30;
+
+
+
+            /////////////////////////////////////////////////////////////////////////
+
+            double C2 = 0;
+            C = Cj;
+            m = m0;
+            chart2.Series[0].Points.Clear();
+            for (double l = 0; l < L; l = l + 0.5)
+            {
+                chart2.Series[0].Points.AddXY(l, C2);
+
+                C = C + d_C1(C, m) * 0.5;
+                C2 = C2 + d_C2(C, C2, m) * 0.5;
+
+            }
+            C2 = 0;
+            C = Cj;
+            m = m + 0.15;
+            chart2.Series[1].Points.Clear();
+            for (double l = 0; l < L; l = l + 0.5)
+            {
+                chart2.Series[1].Points.AddXY(l, C2);
+                C = C + d_C1(C, m) * 0.5;
+                C2 = C2 + d_C2(C, C2, m) * 0.5;
+
+            }
+
+            C2 = 0;
+            C = Cj;
+            m = m + 0.5;
+            chart2.Series[2].Points.Clear();
+            for (double l = 0; l < L; l = l + 0.5)
+            {
+                chart2.Series[2].Points.AddXY(l, C2);
+                C = C + d_C1(C, m) * 0.5;
+                C2 = C2 + d_C2(C,C2, m) * 0.5;
+
+            }
+           //chart2.ChartAreas[0].AxisX.Maximum = 30;
         }
     }
 }
